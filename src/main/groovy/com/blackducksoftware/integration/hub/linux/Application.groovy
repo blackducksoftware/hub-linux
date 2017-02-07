4d987fde88b2dccf51ce72845018c88f0e284b7c
Eric Kerwin <ekerwin@blackducksoftware.com>
2017-02-07 12:52:47 -0500

    adding hub connectivity

--- src/main/groovy/com/blackducksoftware/integration/hub/linux/Application.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/linux/Application.groovy
@@ -4,6 +4,7 @@ import javax.annotation.PostConstruct
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.context.annotation.Bean
@@ -14,13 +15,23 @@ import com.blackducksoftware.bdio.model.ExternalIdentifierBuilder
 class Application {
     private final Logger logger = LoggerFactory.getLogger(Application.class)
 
+    @Autowired
+    HubClient hubClient
+
     static void main(final String[] args) {
         new SpringApplicationBuilder(Application.class).logStartupInfo(false).run(args)
     }
 
     @PostConstruct
     void init() {
-        println "Hello Team!!!"
+        println 'Hello Team!!!'
+
+                try {
+            hubClient.testHubConnection()
+            println 'Your Hub configuration is valid and a successful connection to the Hub was established.'
+        } catch (Exception e) {
+            println("Your Hub configuration is not valid: ${e.message}")
+        }
     }
 
     @Bean
