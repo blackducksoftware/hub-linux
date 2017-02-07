9b7200e9066fec922bb3ab986e5b7c5a5c528f9e
jrichard <jrichard@blackducksoftware.com>
2017-02-07 16:07:20 -0500

    Fixing tests

--- src/test/groovy/com/blackducksoftware/integration/hub/linux/extractor/YumExtractorTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/linux/extractor/YumExtractorTest.groovy
@@ -19,17 +19,21 @@ class YumExtractorTest {
 
     @Test
     public void extractYumComponentsFile1(){
-        URL url = this.getClass().getResource("/centos_yum_output_1.txt")
-        File file = new File(URLDecoder.decode(url.getFile(), "UTF-8"));
-
-        YumExtractor extractor = new YumExtractor()
-        List<BdioComponentDetails> componentDetails =  extractor.extract(file)
-        println(componentDetails.size())
+        extractYumComponentsFromFile("centos_yum_output_1.txt")
     }
 
     @Test
     public void extractYumComponentsFile2(){
-        URL url = this.getClass().getResource("/centos_yum_output_2.txt")
+        extractYumComponentsFromFile("centos_yum_output_2.txt")
+    }
+
+    @Test
+    public void extractYumComponentsFile3(){
+        extractYumComponentsFromFile("centos_yum_output_3.txt")
+    }
+
+    public void extractYumComponentsFromFile(String fileName){
+        URL url = this.getClass().getResource("/$fileName")
         File file = new File(URLDecoder.decode(url.getFile(), "UTF-8"));
 
         YumExtractor extractor = new YumExtractor()
