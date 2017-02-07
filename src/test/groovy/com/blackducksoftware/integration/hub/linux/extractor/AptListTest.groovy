d24babd366be541ab7950aa78f7da92181538571
jrichard <jrichard@blackducksoftware.com>
2017-02-07 15:59:56 -0500

    Updating apt-get tests

--- src/test/groovy/com/blackducksoftware/integration/hub/linux/extractor/AptListTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/linux/extractor/AptListTest.groovy
@@ -2,13 +2,25 @@ package com.blackducksoftware.integration.hub.linux.extractor;
 
 import org.junit.Test
 
+import com.blackducksoftware.integration.hub.linux.BdioComponentDetails
+
 class AptListTest {
 
     @Test
-    void testExtractingApt() {
+    void testExtractingAptFile1() {
         AptExtractor extractor = new AptExtractor()
-        URL url = this.getClass().getResource("/apt_package_list.txt")
+        URL url = this.getClass().getResource("/ubuntu_apt_package_list_1.txt")
         File file = new File(URLDecoder.decode(url.getFile(), "UTF-8"))
-        extractor.extract(file);
+        List<BdioComponentDetails> componentDetails =extractor.extract(file);
+        println(componentDetails.size())
+    }
+
+    @Test
+    void testExtractingAptFile2() {
+        AptExtractor extractor = new AptExtractor()
+        URL url = this.getClass().getResource("/ubuntu_apt_package_list_2.txt")
+        File file = new File(URLDecoder.decode(url.getFile(), "UTF-8"))
+        List<BdioComponentDetails> componentDetails =extractor.extract(file);
+        println(componentDetails.size())
     }
 }
