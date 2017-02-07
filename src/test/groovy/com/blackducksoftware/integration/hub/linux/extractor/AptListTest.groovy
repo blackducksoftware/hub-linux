21d78351ea27ea77b55ff47ea6f1b8e46c12b645
jrichard <jrichard@blackducksoftware.com>
2017-02-07 15:51:37 -0500

    Fixing AptListTest

--- src/test/groovy/com/blackducksoftware/integration/hub/linux/extractor/AptListTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/linux/extractor/AptListTest.groovy
@@ -8,7 +8,7 @@ class AptListTest {
     void testExtractingApt() {
         AptExtractor extractor = new AptExtractor()
         URL url = this.getClass().getResource("/apt_package_list.txt")
-        File file = new File(url.getFile())
+        File file = new File(URLDecoder.decode(url.getFile(), "UTF-8"))
         extractor.extract(file);
     }
 }
